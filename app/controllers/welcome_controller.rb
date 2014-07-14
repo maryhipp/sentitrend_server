91513e22c61721b6aeddb152313f398e0e11f633
maryhipp <maryhipp@gmail.com>
2014-07-14 16:22:20 -0400

    took our pry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,6 @@ class WelcomeController < ApplicationController
   end
 
   def analyze
-    binding.pry
     url = CGI::escape(params[:url])
     response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?url=" + url + "&apikey=" + ENV['ALCHEMY_API_KEY'] + "&outputMode=jsonp&sentiment=1&maxRetrieve=15&jsonp=MyCallback")
 
