e29e2ea916f778464604006befe668884a690803
maryhipp <maryhipp@gmail.com>
2014-07-14 16:29:53 -0400

    json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,7 @@ class WelcomeController < ApplicationController
     url = CGI::escape(params[:url])
     response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?url=" + url + "&apikey=" + ENV['ALCHEMY_API_KEY'] + "&outputMode=jsonp&sentiment=1&maxRetrieve=15&jsonp=MyCallback")
 
-    render jsonp: response
+    render json: response
 
   end
 end
