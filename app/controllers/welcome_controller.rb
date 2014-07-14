c338e9a3d002cbc67c7fa899130ae787a81d5e71
maryhipp <maryhipp@gmail.com>
2014-07-14 16:06:05 -0400

    api working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
+
   def analyze
-    response = httparty.get("https://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?url=" + params[:url] + "&apikey=" + ENV['ALCHEMY_API_KEY'] + "&outputMode=json&sentiment=1&maxRetrieve=15")
+    binding.pry
+    url = CGI::escape(params[:url])
+    response = HTTParty.get("http://access.alchemyapi.com/calls/url/URLGetRankedNamedEntities?url=" + url + "&apikey=" + ENV['ALCHEMY_API_KEY'] + "&outputMode=jsonp&sentiment=1&maxRetrieve=15&jsonp=MyCallback")
+
     render json: response
 
   end
